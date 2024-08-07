35f59924f14b96d0bb6c5e5092afcc302f4dfcf9
namkidong98 <rlehd201@naver.com>
2024-08-07 10:32:55 +0900

    서버 파일로 최종 업데이트

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,re_path
-from accounts.views import KakaoLoginView
+from django.urls import path, include
+# from accounts.views import KakaoLoginView
 from django.conf import settings
 from django.conf.urls.static import static
 
